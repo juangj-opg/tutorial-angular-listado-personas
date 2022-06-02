eceb029dc9ad39b4f3430627c5e1fb85fceef868
Juan Garrido <juangj1b@gmail.com>
2022-06-02 11:20:15 +0200

    Tutorial Angular - S7 66 - Enrutamiento y CRUD (Falta Delete)

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from '../persona.model';
+import { PersonasService } from '../personas.service';
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent implements OnInit {
+
+  personas: Persona[] = [];
+
+  constructor(
+    private personasService: PersonasService,
+    private router:Router,
+  ) {}
+
+  ngOnInit() {
+    this.personas = this.personasService.personas;
+  }
+
+  agregar() {
+    this.router.navigate(['personas/agregar'])
+  }
+}
