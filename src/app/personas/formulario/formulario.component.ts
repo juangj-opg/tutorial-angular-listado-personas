7feaeeca70abe7c0ea3f4b15702a6fa39b4e9e04
Juan Garrido <juangj1b@gmail.com>
2022-06-02 13:18:56 +0200

    Tutorial Angular - S8 67 - Parametros Query

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -12,6 +12,7 @@ export class FormularioComponent implements OnInit {
   nombreInput: string = '';
   apellidoInput: string = '';
   index: number;
+  modoEdicion: number;
 
   constructor(
     private personasService: PersonasService,
@@ -25,7 +26,9 @@ export class FormularioComponent implements OnInit {
 
   ngOnInit() {
     this.index = this.route.snapshot.params['id'];
-    if(this.index) {
+    this.modoEdicion = +this.route.snapshot.queryParams['modoEdicion'];
+    
+    if(this.modoEdicion != null && this.modoEdicion == 1) {
       let persona: Persona = this.personasService.encontrarPersona(this.index);
       this.nombreInput = persona.nombre;
       this.apellidoInput = persona.apellido;
@@ -34,10 +37,10 @@ export class FormularioComponent implements OnInit {
 
   agregarPersona() {
     let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    if (this.index){
+    if (this.modoEdicion != null && this.modoEdicion == 1){
       this.personasService.modificarPersona(this.index, persona1);
     } else {
-      this.personasService.agregarPersona(persona1);
+      this.personasService.agregarPersona(persona1); 
     }
     this.router.navigate(['personas']);
   }
