0db7754987536477cb70bd85926a63e117c65460
Juan Garrido <juangj1b@gmail.com>
2022-06-03 14:30:52 +0200

    Tutorial Angular - S10 80 - Login Guardian

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from '../error/error.component';
+import { LoginGuardian } from '../login/login-guardian.service';
 import { LoginComponent } from '../login/login.component';
 import { FormularioComponent } from '../personas/formulario/formulario.component';
 import { PersonasComponent } from '../personas/personas.component';
 
 const routes: Routes = [
-  {path: '', component: PersonasComponent},
-  {path: 'personas', component: PersonasComponent, children: [
+  {path: '', component: PersonasComponent, canActivate:[LoginGuardian]},
+  {path: 'personas', component: PersonasComponent, canActivate:[LoginGuardian] ,children: [
     {path: 'agregar', component: FormularioComponent},
     {path: ':id', component: FormularioComponent},
   ]},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DataServices } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/login.service';
+import { LoginGuardian } from './login/login-guardian.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { LoginService } from './login/login.service';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [LoggingService, PersonasService, DataServices, LoginService],
+  providers: [LoggingService, PersonasService, DataServices, LoginService, LoginGuardian],
   bootstrap: [AppComponent]
 })
 export class AppModule { 
